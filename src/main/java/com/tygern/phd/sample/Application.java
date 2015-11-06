2d62d3b10c7b56764c31d43c86260d95ccf022dc
Fabio Mangione and Tyson Gern <pair+fmangione+tgern@pivotal.io>
2015-11-06 17:19:51 +0000

    initial commit

---
+++ src/main/java/com/tygern/phd/sample/Application.java
@@ -0,0 +1,31 @@
+package com.tygern.phd.sample;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Map;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    private Logger logger = LoggerFactory.getLogger(Application.class);
+
+    @Autowired
+    private JdbcTemplate _jdbcTemplate;
+
+    public static void main(String args[]) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("Connecting to database");
+        Map<String, Object> resultMap = _jdbcTemplate.queryForMap("select now();");
+        logger.info( "Found " + resultMap.size() + " results" );
+    }
+}
